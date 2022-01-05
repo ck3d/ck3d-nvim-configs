e18b4978a78dac51fe36b8e8b9131b47bb05ea31
Christian Kögler <ck3d@gmx.de>
2022-01-05 17:02:13 +0100

    Add statix

--- config.nix
+++ config.nix
@@ -295,6 +295,10 @@ in
             "formatting.lua_format.with({command = '${pkgs.luaformatter}/bin/lua-format'})"
           ++ lib.optional (hasLang "beancount")
             "formatting.bean_format"
+          ++ lib.optionals (hasLang "nix") [
+            "code_actions.statix.with({command = '${pkgs.statix}/bin/statix'})"
+            "diagnostics.statix.with({command = '${pkgs.statix}/bin/statix'})"
+          ]
         );
         on_attach = ./lspconfig-on_attach.lua;
       };
