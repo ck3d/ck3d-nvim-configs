902dd81bb20cb9d86f34172c8daa4d2c2b4c83c0
Christian Kögler <ck3d@gmx.de>
2022-04-17 16:49:38 +0200

    lemminx: 0.19.1 -> 0.20.0

--- pkgs/lemminx/default.nix
+++ pkgs/lemminx/default.nix
@@ -5,10 +5,10 @@
 }:
 let
   pname = "lemminx";
-  version = "0.19.1";
+  version = "0.20.0";
   jar = fetchurl {
     url = "https://repo.eclipse.org/content/repositories/lemminx-releases/org/eclipse/lemminx/org.eclipse.lemminx/${version}/org.eclipse.lemminx-${version}-uber.jar";
-    sha256 = "sha256-nHxMLeCAVZXyMEnIDA7PSTMEKOUfUzrl5Su7onnptik=";
+    sha256 = "sha256-/Tc1BkNoecyPXn/JZastYR14TB6lFe6T7BtMsVMD5KI=";
   };
 in
 runCommandLocal "${pname}-${version}"
