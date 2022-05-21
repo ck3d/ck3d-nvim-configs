7567cf71a7e9325b986cb04b9f1ccb8ce1e1924e
Christian Kögler <ck3d@gmx.de>
2022-05-21 08:13:46 +0200

    cmp-nvim-tags: Update version

--- pkgs/vimPlugins/default.nix
+++ pkgs/vimPlugins/default.nix
@@ -7,12 +7,12 @@ in
 {
   cmp-nvim-tags = buildVimPluginFrom2Nix rec {
     pname = "cmp-nvim-tags";
-    version = "20210916";
+    version = "20220331";
     src = fetchFromGitHub {
       owner = "quangnguyen30192";
       repo = pname;
-      rev = "51cb13c2a649d81cd88cbd00654b0998d1ee7fa6";
-      sha256 = "sha256-Vu636QTwO/Bm7WDyMnJx7AM2+Co2Fp3i1cB48C7du7w=";
+      rev = "98b15fee0cd64760345be3a30f1a592b5a9abb20";
+      sha256 = "sha256-RwRL18inq2X70Cq78rXa50xUX419RoAPoPmrq7r6z6U=";
     };
     meta.homepage = "https://github.com/quangnguyen30192/cmp-nvim-tags";
   };
