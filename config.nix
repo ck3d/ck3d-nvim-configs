221a157dd7fa41c8897474122d7e0bec70bb749d
Christian Kögler <ck3d@gmx.de>
2022-11-23 21:50:36 +0100

    nvim-tree: Add project.nvim settings

--- config.nix
+++ config.nix
@@ -129,7 +129,15 @@ in
 
     nvim-tree = {
       plugins = [ vimPlugins.nvim-tree-lua ];
-      setup = { };
+      setup.args = {
+        # https://github.com/ahmedkhalf/project.nvim
+        sync_root_with_cwd = true;
+        respect_buf_cwd = true;
+        update_focused_file = {
+          enable = true;
+          update_root = true;
+        };
+      };
       keymaps = map keymap_silent [
         [ "n" "<C-n>" "<Cmd>NvimTreeToggle<CR>" { } ]
       ];
