9bb42786b6cbff736570a15ffe622beec42eefe3
Christian Kögler <ck3d@gmx.de>
2022-12-11 13:24:46 +0100

    sumneko: Disable formatting

--- sumneko_lua.config.lua
+++ sumneko_lua.config.lua
@@ -2,6 +2,10 @@ return {
   runtime = {version = "LuaJIT"},
   path = vim.split(package.path, ';'),
   diagnostics = {enable = true, globals = {"vim"}},
+  format = {
+    -- use lua-format
+    enable = false,
+  },
   workspace = {
     library = (function()
       local result = {
