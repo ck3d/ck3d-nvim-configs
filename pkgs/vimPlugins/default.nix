a13984e975afc9502cc6ff3050137268e0bec1ad
Christian Kögler <ck3d@gmx.de>
2022-12-11 13:38:18 +0100

    nvim-osc52: 2022-11-10 -> 2022-11-22

--- pkgs/vimPlugins/default.nix
+++ pkgs/vimPlugins/default.nix
@@ -7,12 +7,12 @@ in
 {
   nvim-osc52 = buildVimPluginFrom2Nix {
     pname = "nvim-osc52";
-    version = "2022-11-10";
+    version = "2022-11-22";
     src = fetchFromGitHub {
       owner = "ojroques";
       repo = "nvim-osc52";
-      rev = "27b922a88aba9b2533c4a0e0bc5bca65e3405739";
-      sha256 = "sha256-ESqihzkYFsKOtAwg9tn5k41BHuSbHGKDnxPBLLtIro8=";
+      rev = "5e7efbc047be9eca1307899137a89cd2b6b8125a";
+      sha256 = "sha256-tr0MeOdXf9ndJpdoBy8vyahuPdwXk94JyEQO3ppzfkU=";
     };
   };
 }
