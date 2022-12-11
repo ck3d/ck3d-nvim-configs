d991b4aec47ac75802013c268416806e72b1779a
Christian Kögler <ck3d@gmx.de>
2022-12-11 13:17:23 +0100

    cmp_setup_args: Apply lua-format

--- cmp_setup_args.lua
+++ cmp_setup_args.lua
@@ -1,20 +1,13 @@
 -- https://github.com/hrsh7th/nvim-cmp/blob/main/lua/cmp/config/default.lua
 return {
-  snippet = { expand = function(args)
-    vim.fn['vsnip#anonymous'](args.body)
-  end },
-  sources = require 'cmp'.config.sources({
-    { name = "nvim_lua" },
-    { name = "nvim_lsp" },
-    { name = "treesitter" },
-    { name = "omni" },
-    { name = "vsnip" },
-    { name = "path", keyword_length = 2 },
-    { name = "tags", keyword_length = 4 },
-    { name = "buffer", keyword_length = 4 },
-    { name = "spell", keyword_length = 4 },
-  }, { { name = "buffer" } }),
-  mapping = require 'cmp'.mapping.preset.insert({
-    ['<CR>'] = require 'cmp'.mapping.confirm({ select = true }),
+  snippet = {expand = function(args) vim.fn['vsnip#anonymous'](args.body) end},
+  sources = require'cmp'.config.sources({
+    {name = "nvim_lua"}, {name = "nvim_lsp"}, {name = "treesitter"},
+    {name = "omni"}, {name = "vsnip"}, {name = "path", keyword_length = 2},
+    {name = "tags", keyword_length = 4}, {name = "buffer", keyword_length = 4},
+    {name = "spell", keyword_length = 4},
+  }, {{name = "buffer"}}),
+  mapping = require'cmp'.mapping.preset.insert({
+    ['<CR>'] = require'cmp'.mapping.confirm({select = true}),
   }),
 }
