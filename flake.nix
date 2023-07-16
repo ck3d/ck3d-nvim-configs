0fa6e7da16473f6d5c5ba0e9a5018041dcd7355b
Christian Kögler <ck3d@gmx.de>
2023-07-16 10:10:57 +0200

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/f3a2296445df5ca6b8aa6aac6132365c0735c751' (2023-07-04)
      → 'github:NixOS/nixpkgs/3368e885d8ce16ff7de272114d773a9514760358' (2023-07-15)
    • Removed input 'utils'
    • Removed input 'utils/systems'

--- flake.nix
+++ flake.nix
@@ -4,95 +4,96 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     nix2nvimrc.url = "github:ck3d/nix2nvimrc";
-    utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils, nix2nvimrc }:
+  outputs = { self, nixpkgs, nix2nvimrc }:
     let
-      overlays.default = import ./pkgs/overlay.nix;
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
     in
-    { inherit overlays; }
-    // (utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = builtins.attrValues overlays;
+    rec {
+      overlays.default = import ./pkgs/overlay.nix;
 
-          config.permittedInsecurePackages = [
-            "openssl-1.1.1u"
-          ];
-        };
-        adminLanguages = [
-          "nix"
-          "yaml"
-          "bash"
-          "lua"
-          # activate after merge of
-          "markdown"
-          "json"
-          "toml"
-        ];
-        nvim = name: languages:
+      packages = forAllSystems
+        (system:
           let
-            rc = pkgs.writeText
-              ("nvimrc-" + name)
-              (nix2nvimrc.lib.toRc pkgs {
-                inherit languages;
-                imports = [ ./config.nix ];
-              });
-            mainProgram = "nvim";
-          in
-          pkgs.runCommandLocal name
-            {
-              nativeBuildInputs = [
-                pkgs.makeWrapper
-                # needed by plugin gitsigns
-                pkgs.gitMinimal
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = builtins.attrValues overlays;
+
+              config.permittedInsecurePackages = [
+                "openssl-1.1.1u"
               ];
-              meta = { inherit mainProgram; };
-            }
-            ''
-              makeWrapper ${pkgs.neovim-unwrapped}/bin/nvim $out/bin/${mainProgram} \
-                --add-flags "-u NORC --cmd 'luafile ${rc}'"
-              HOME=$(pwd) $out/bin/nvim --headless +"q" 2> err
-              if [ -s err ]; then
-                cat err
-                false
-              fi
-            ''
-        ;
+            };
+            adminLanguages = [
+              "nix"
+              "yaml"
+              "bash"
+              "lua"
+              # activate after merge of
+              "markdown"
+              "json"
+              "toml"
+            ];
+            nvim = name: languages:
+              let
+                rc = pkgs.writeText
+                  ("nvimrc-" + name)
+                  (nix2nvimrc.lib.toRc pkgs {
+                    inherit languages;
+                    imports = [ ./config.nix ];
+                  });
+                mainProgram = "nvim";
+              in
+              pkgs.runCommandLocal name
+                {
+                  nativeBuildInputs = [
+                    pkgs.makeWrapper
+                    # needed by plugin gitsigns
+                    pkgs.gitMinimal
+                  ];
+                  meta = { inherit mainProgram; };
+                }
+                ''
+                  makeWrapper ${pkgs.neovim-unwrapped}/bin/nvim $out/bin/${mainProgram} \
+                    --add-flags "-u NORC --cmd 'luafile ${rc}'"
+                  HOME=$(pwd) $out/bin/nvim --headless +"q" 2> err
+                  if [ -s err ]; then
+                    cat err
+                    false
+                  fi
+                ''
+            ;
 
-        nvims = builtins.mapAttrs nvim {
-          nvim-admin = adminLanguages;
-          nvim-dev = adminLanguages ++ [
-            # treesitter
-            "rust"
-            "beancount"
-            "javascript"
-            "html"
-            "c"
-            "cpp"
-            "css"
-            "make"
-            "graphql"
-            "python"
-            "scheme"
-            "latex"
-            "devicetree"
-            "go"
-            "dhall"
-            "jq"
-            "vue"
-            # no treesitter
-            "xml"
-            "plantuml"
-          ];
-        };
-
-      in
-      {
-        packages = nvims
-        // { default = nvims.nvim-admin; }
-        // pkgs.ck3dNvimPkgs;
-      }));
+            nvims = builtins.mapAttrs nvim {
+              nvim-admin = adminLanguages;
+              nvim-dev = adminLanguages ++ [
+                # treesitter
+                "rust"
+                "beancount"
+                "javascript"
+                "html"
+                "c"
+                "cpp"
+                "css"
+                "make"
+                "graphql"
+                "python"
+                "scheme"
+                "latex"
+                "devicetree"
+                "go"
+                "dhall"
+                "jq"
+                "vue"
+                # no treesitter
+                "xml"
+                "plantuml"
+              ];
+            };
+          in
+          nvims
+          // { default = nvims.nvim-admin; }
+          // pkgs.ck3dNvimPkgs
+        );
+    };
 }
