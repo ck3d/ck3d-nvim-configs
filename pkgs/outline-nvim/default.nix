fc97857843152dd2633cbad65aa411abd4b69944
Christian Kögler <ck3d@gmx.de>
2024-04-02 22:39:25 +0200

    outline-nvim: Update to latest version

--- pkgs/outline-nvim/default.nix
+++ pkgs/outline-nvim/default.nix
@@ -3,11 +3,11 @@
 }:
 vimUtils.buildVimPlugin {
   pname = "outline.nvim";
-  version = "2024-01-24";
+  version = "2024-03-16";
   src = fetchFromGitHub {
     owner = "hedyhli";
     repo = "outline.nvim";
-    rev = "a8d40aecb799196303ff3521c0e31c87bba57198";
-    hash = "sha256-HaxfnvgFy7fpa2CS7/dQhf6dK9+Js7wP5qGdIeXLGPY=";
+    rev = "bdfd2da90e9a7686d00e55afa9f772c4b6809413";
+    hash = "sha256-Il27Z/vQVJ3WSrFnTOUwBeAFCjopXwDhdsi7A5v1gzU=";
   };
 }
