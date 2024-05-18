b787c8c9993f8aca0ebd7a7e773e134153ed51d3
Christian Kögler <ck3d@gmx.de>
2024-05-18 10:54:27 +0200

    flake: Use self

--- flake.nix
+++ flake.nix
@@ -11,22 +11,21 @@
       inherit (nixpkgs) lib;
       forAllSystems = lib.genAttrs lib.systems.flakeExposed;
       inherit (import ./lib.nix lib) readDirNix;
-
-      overlays.default = import ./overlay.nix;
-      nix2nvimrcModules = readDirNix ./modules;
-      nix2nvimrcConfigs = readDirNix ./configs;
     in
     {
       lib = import ./lib.nix;
 
-      inherit overlays nix2nvimrcModules nix2nvimrcConfigs;
+      overlays.default = import ./overlay.nix;
+
+      nix2nvimrcModules = readDirNix ./modules;
+      nix2nvimrcConfigs = readDirNix ./configs;
 
       packages = forAllSystems
         (system:
           let
             pkgs = import nixpkgs {
               inherit system;
-              overlays = builtins.attrValues overlays;
+              overlays = builtins.attrValues self.overlays;
             };
 
             adminLanguages = [
@@ -44,8 +43,8 @@
                 (lib.evalModules {
                   modules =
                     (nix2nvimrc.lib.modules pkgs)
-                    ++ (builtins.attrValues nix2nvimrcModules)
-                    ++ (builtins.attrValues nix2nvimrcConfigs)
+                    ++ (builtins.attrValues self.nix2nvimrcModules)
+                    ++ (builtins.attrValues self.nix2nvimrcConfigs)
                     ++ [{
                       wrapper.name = name;
                       inherit languages;
