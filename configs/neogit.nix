44d1026d947c69d4f5fac81e140935294d0aa345
Christian Kögler <ck3d@gmx.de>
2024-05-19 18:46:38 +0200

    neogit: Pin version for compatibility to nvim 0.9

--- configs/neogit.nix
+++ configs/neogit.nix
@@ -1,7 +1,16 @@
 { config, pkgs, ... }:
 {
   configs.neogit = {
-    plugins = [ pkgs.vimPlugins.neogit ];
+    plugins = [ (pkgs.vimPlugins.neogit.overrideAttrs (old: {
+      src = pkgs.fetchFromGitHub {
+        owner = "NeogitOrg";
+        repo = "neogit";
+        # compatible with neovim 0.9
+        # https://github.com/NeogitOrg/neogit/releases/tag/v0.0.1
+        rev = "v0.0.1";
+        hash = "sha256-xncUButV2/FSOPNolbrZuPPSC1FOtgrlZBwzHHPKCTo=";
+      };
+    })) ];
     setup.args = {
       integrations.diffview = config.config ? diffview;
       integrations.telescope = config.config ? telescope;
