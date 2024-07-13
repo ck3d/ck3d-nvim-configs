0d7c55bdbec152d22ac337a5f2c0f6866585cd9c
Christian Kögler <ck3d@gmx.de>
2024-07-13 14:01:13 +0200

    outline: Use package from nixpkgs

--- configs/outline.nix
+++ configs/outline.nix
@@ -1,7 +1,7 @@
 { pkgs, nix2nvimrc, ... }:
 {
   configs.outline = {
-    plugins = [ pkgs.ck3dNvimPkgs.outline-nvim ];
+    plugins = [ pkgs.vimPlugins.outline-nvim ];
     setup = { };
     keymaps = map (nix2nvimrc.toKeymap { silent = true; }) [
       [ "n" "<C-o>" "<Cmd>Outline<CR>" { desc = "Toggle Outline"; } ]
