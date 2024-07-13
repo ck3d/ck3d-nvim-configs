4dc9f171db4e23c537b870e89d5de51c137119b9
Christian Kögler <ck3d@gmx.de>
2024-07-13 14:04:26 +0200

    neogit: Use package from nixpkgs

--- configs/neogit.nix
+++ configs/neogit.nix
@@ -1,16 +1,7 @@
 { config, pkgs, ... }:
 {
   configs.neogit = {
-    plugins = [ (pkgs.vimPlugins.neogit.overrideAttrs (old: {
-      src = pkgs.fetchFromGitHub {
-        owner = "NeogitOrg";
-        repo = "neogit";
-        # compatible with neovim 0.9
-        # https://github.com/NeogitOrg/neogit/releases/tag/v0.0.1
-        rev = "v0.0.1";
-        hash = "sha256-xncUButV2/FSOPNolbrZuPPSC1FOtgrlZBwzHHPKCTo=";
-      };
-    })) ];
+    plugins = [ pkgs.vimPlugins.neogit ];
     setup.args = {
       integrations.diffview = config.config ? diffview;
       integrations.telescope = config.config ? telescope;
