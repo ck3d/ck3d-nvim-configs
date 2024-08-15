695ec8d4d9aab26253592338359998c73d740c3e
Christian Kögler <ck3d@gmx.de>
2024-08-15 11:37:09 +0200

    null-ls: Add prettier only for javascript

--- configs/null-ls.nix
+++ configs/null-ls.nix
@@ -16,10 +16,10 @@ in
       sources = map (s: nix2nvimrc.luaExpr ("require'null-ls.builtins'." + s)) (
         [
           "code_actions.gitsigns"
-          "formatting.prettier"
           "diagnostics.proselint"
         ]
         ++ lib.optionals (hasLang "markdown") [ "diagnostics.markdownlint_cli2" ]
+        ++ lib.optionals (hasLang "javascript") [ "formatting.prettier" ]
         ++ lib.optionals (hasLang "nix") [
           "code_actions.statix"
           "diagnostics.statix"
@@ -28,11 +28,9 @@ in
       on_attach = ./lspconfig-on_attach.lua;
     };
     env.PATH.values =
-      [
-        "${pkgs.proselint}/bin"
-        "${pkgs.nodePackages.prettier}/bin"
-      ]
+      [ "${pkgs.proselint}/bin" ]
       ++ lib.optional (hasLang "markdown") "${pkgs.markdownlint-cli2}/bin"
+      ++ lib.optional (hasLang "javascript") "${pkgs.nodePackages.prettier}/bin"
       ++ lib.optional (hasLang "nix") "${pkgs.statix}/bin";
   };
 }
