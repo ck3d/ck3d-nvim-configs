8808326b3ae809940d7805fbb0e047b9a6baa58b
Christian Kögler <ck3d@gmx.de>
2024-08-15 11:48:56 +0200

    null-ls: Add proselint only for markdown

--- configs/null-ls.nix
+++ configs/null-ls.nix
@@ -14,11 +14,11 @@ in
     plugins = [ pkgs.vimPlugins.none-ls-nvim ];
     setup.args = {
       sources = map (s: nix2nvimrc.luaExpr ("require'null-ls.builtins'." + s)) (
-        [
-          "code_actions.gitsigns"
+        [ "code_actions.gitsigns" ]
+        ++ lib.optionals (hasLang "markdown") [
           "diagnostics.proselint"
+          "diagnostics.markdownlint_cli2"
         ]
-        ++ lib.optionals (hasLang "markdown") [ "diagnostics.markdownlint_cli2" ]
         ++ lib.optionals (hasLang "javascript") [ "formatting.prettier" ]
         ++ lib.optionals (hasLang "nix") [
           "code_actions.statix"
@@ -28,8 +28,10 @@ in
       on_attach = ./lspconfig-on_attach.lua;
     };
     env.PATH.values =
-      [ "${pkgs.proselint}/bin" ]
-      ++ lib.optional (hasLang "markdown") "${pkgs.markdownlint-cli2}/bin"
+      lib.optionals (hasLang "markdown") [
+        "${pkgs.markdownlint-cli2}/bin"
+        "${pkgs.proselint}/bin"
+      ]
       ++ lib.optional (hasLang "javascript") "${pkgs.nodePackages.prettier}/bin"
       ++ lib.optional (hasLang "nix") "${pkgs.statix}/bin";
   };
