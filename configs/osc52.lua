dd041f075f5fd9c5c3f10604d188db9c639927dd
Christian Kögler <ck3d@gmx.de>
2025-03-09 18:14:16 +0100

    osc52: Use builtin feature

---
+++ configs/osc52.lua
@@ -0,0 +1,12 @@
+-- see help clipboard-osc52
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
--- configs/osc52.nix
+++ configs/osc52.nix
@@ -1,25 +1,7 @@
-{ pkgs, nix2nvimrc, ... }:
+{ nix2nvimrc, ... }:
+let
+  inherit (nix2nvimrc) toLua;
+in
 {
-  configs.osc52 = {
-    after = [ "leader" ];
-    plugins = [ pkgs.vimPlugins.nvim-osc52 ];
-    setup = { };
-    keymaps = map (nix2nvimrc.toKeymap { }) [
-      [
-        "n"
-        "<Leader>y"
-        (nix2nvimrc.luaExpr "require'osc52'.copy_operator")
-        {
-          expr = true;
-          desc = "Yank to clipboard";
-        }
-      ]
-      [
-        "x"
-        "<Leader>y"
-        (nix2nvimrc.luaExpr "require'osc52'.copy_visual")
-        { desc = "Yank to clipboard"; }
-      ]
-    ];
-  };
+  configs.osc52.lua = [ (toLua ./osc52.lua) ];
 }
