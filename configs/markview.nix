9052a8ddeeaaa0616dba6e98d5d4da2776caa618
Christian Kögler <ck3d@gmx.de>
2025-07-26 18:23:16 +0200

    markview: Disable preview per default

--- configs/markview.nix
+++ configs/markview.nix
@@ -1,7 +1,20 @@
-{ pkgs, ... }:
+{ pkgs, nix2nvimrc, ... }:
 {
   configs.markview = {
     plugins = [ pkgs.vimPlugins.markview-nvim ];
-    setup = { };
+    setup.args = {
+      # https://github.com/OXY2DEV/markview.nvim/wiki/Preview
+      preview.enable = false;
+    };
+    keymaps = map (nix2nvimrc.toKeymap { }) [
+      [
+        "n"
+        "<Leader>M"
+        "<Cmd>Markview<CR>"
+        {
+          desc = "Toggle Markview";
+        }
+      ]
+    ];
   };
 }
