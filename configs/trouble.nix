e4513bd131c59bef244726b1d76b61a1d05d94e5
Christian Kögler <ck3d@gmx.de>
2026-02-24 22:37:08 +0100

    trouble: Fix keymaps

--- configs/trouble.nix
+++ configs/trouble.nix
@@ -14,15 +14,39 @@
     keymaps = map (nix2nvimrc.toKeymap { silent = true; }) [
       [
         "n"
-        "<Leader>xx"
-        "<Cmd>TroubleToggle<CR>"
-        { desc = "Toggle trouble list"; }
+        "<leader>xx"
+        "<cmd>Trouble diagnostics toggle<cr>"
+        { desc = "Diagnostics (Trouble)"; }
       ]
       [
         "n"
-        "gR"
-        "<Cmd>TroubleToggle lsp_references<CR>"
-        { desc = "Toggle lsp reference trouble list"; }
+        "<leader>xX"
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>"
+        { desc = "Buffer Diagnostics (Trouble)"; }
+      ]
+      [
+        "n"
+        "<leader>cs"
+        "<cmd>Trouble symbols toggle focus=false<cr>"
+        { desc = "Symbols (Trouble)"; }
+      ]
+      [
+        "n"
+        "<leader>cl"
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>"
+        { desc = "LSP Definitions / references / ... (Trouble)"; }
+      ]
+      [
+        "n"
+        "<leader>xL"
+        "<cmd>Trouble loclist toggle<cr>"
+        { desc = "Location List (Trouble)"; }
+      ]
+      [
+        "n"
+        "<leader>xQ"
+        "<cmd>Trouble qflist toggle<cr>"
+        { desc = "Quickfix List (Trouble)"; }
       ]
     ];
   };
