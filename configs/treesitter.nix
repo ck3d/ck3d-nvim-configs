b4fd217ff03fd1fde4100f6378ac774f47f54d4c
Christian Kögler <ck3d@gmx.de>
2026-06-04 15:35:36 +0200

    treesitter: Simplified

--- configs/treesitter.nix
+++ configs/treesitter.nix
@@ -12,36 +12,32 @@
         # and it misses languages like jq, vimdoc, and xml
         grammars = pkgs.vimPlugins.nvim-treesitter.builtGrammars;
 
-        grammars' = lib.getAttrs (builtins.filter
-          (
-            type:
-            if builtins.hasAttr type grammars then
-              true
-            # disable trace for following languages
-            else if builtins.any (lang: type == lang) [ "tree-sitter-plantuml" ] then
-              false
-            else
-              builtins.trace "no tree-sitter parser for language ${type} available" false
-          )
-          (
-            map (lang: "tree-sitter-" + lang) (
-              # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/by-name/ne/neovim-unwrapped/treesitter-parsers.nix
-              lib.subtractLists [
-                "c"
-                "lua"
-                "vim"
-                "vimdoc"
-                "query"
-                "markdown"
-              ] config.languages
-            )
-          )
-        ) grammars;
-      in
-      lib.mapAttrs' (n: v: lib.nameValuePair (lib.removePrefix "tree-sitter-" n) "${v}/parser") grammars';
+        # Exclude built-in parsers (https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/by-name/ne/neovim-unwrapped/treesitter-parsers.nix)
+        excluded = [
+          "c"
+          "lua"
+          "vim"
+          "vimdoc"
+          "query"
+          "markdown"
+        ];
 
-    env.PATH.values = [
-      pkgs.tree-sitter
-    ];
+        languages = lib.subtractLists excluded config.languages;
+
+        makeParserEntry =
+          lang:
+          let
+            name = "tree-sitter-${lang}";
+          in
+          if grammars ? ${name} then
+            [ (lib.nameValuePair lang "${grammars.${name}}/parser") ]
+          else if lang == "plantuml" then
+            [ ] # disable trace for plantuml
+          else
+            builtins.trace "no tree-sitter parser for language ${lang} available" [ ];
+      in
+      lib.listToAttrs (lib.concatMap makeParserEntry languages);
+
+    env.PATH.values = [ pkgs.tree-sitter ];
   };
 }
