7347dc1df6f3da1e1cabb0f2e2328c8f7bf7526b
Christian Kögler <ck3d@gmx.de>
2026-06-06 06:53:40 +0200

    lspconfig: restructure code

--- configs/lspconfig.nix
+++ configs/lspconfig.nix
@@ -8,6 +8,114 @@
 let
   inherit (nix2nvimrc) luaExpr;
   inherit (config) hasLang;
+
+  langs = {
+    bash = {
+      servers.bashls = { };
+      # https://github.com/bash-lsp/bash-language-server?tab=readme-ov-file#dependencies
+      packages = [
+        pkgs.bash-language-server
+        pkgs.shellcheck
+        pkgs.shfmt
+      ];
+    };
+    beancount = {
+      servers.beancount.config.init_options.root_file = "Root.beancount";
+      packages = [ pkgs.beancount-language-server ];
+    };
+    cpp = {
+      servers.clangd = { };
+      packages = [ pkgs.clang-tools ];
+    };
+    css = {
+      servers.cssls = { };
+      packages = [ pkgs.vscode-langservers-extracted ];
+    };
+    go = {
+      servers.gopls = { };
+      packages = [
+        pkgs.gopls
+        pkgs.go
+      ];
+    };
+    html = {
+      servers.html = { };
+      packages = [ pkgs.vscode-langservers-extracted ];
+    };
+    javascript = {
+      servers.ts_ls.config = { };
+      packages = [
+        pkgs.typescript-language-server
+        pkgs.nodejs
+        pkgs.bun
+        pkgs.tsx
+      ];
+    };
+    json = {
+      servers.jsonls = { };
+      packages = [ pkgs.vscode-langservers-extracted ];
+    };
+    lua = {
+      servers.lua_ls.config.settings.Lua = ./sumneko_lua.config.lua;
+      packages = [
+        pkgs.lua-language-server
+      ]
+      ++ lib.optional (config ? wrapper.pkg.lua) config.wrapper.pkg.lua;
+    };
+    nickel = {
+      servers.nickel_ls = { };
+      packages = [
+        pkgs.nls
+        pkgs.nickel
+      ];
+    };
+    nix = {
+      servers.nixd.config = {
+        # https://github.com/nix-community/nixd/blob/main/nixd/docs/configuration.md#where-to-place-the-configuration
+        settings.nixd.formatting.command = [ "nixfmt" ];
+      };
+      packages = [
+        pkgs.nixd
+        pkgs.nixfmt
+      ];
+    };
+    python = {
+      servers.pyright = { };
+      packages = [
+        pkgs.pyright
+        pkgs.python3
+      ];
+    };
+    rust = {
+      servers.rust_analyzer = { };
+      # default config of rust-analyzer expects cargo:
+      # https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/configs/rust_analyzer.lua#L58
+      packages = [
+        pkgs.rust-analyzer
+        pkgs.cargo
+        # since cargo depends on rustc, lets make it available:
+        pkgs.cargo.rustc
+      ];
+    };
+    typst = {
+      servers.tinymist = { };
+      packages = [
+        pkgs.tinymist
+        pkgs.typstyle
+        pkgs.typst
+      ];
+    };
+    xml = {
+      servers.lemminx.config.filetypes = [ "xslt" ];
+      packages = [ pkgs.lemminx ];
+    };
+    yaml = {
+      servers.yamlls = { };
+      packages = [ pkgs.yaml-language-server ];
+    };
+  };
+
+  enabledLangs = lib.filterAttrs (name: _: hasLang name) langs;
 in
 {
   configs.lspconfig = {
@@ -17,35 +125,7 @@ in
     ];
     plugins = [ pkgs.vimPlugins.nvim-lspconfig ];
     lspconfig = {
-      servers =
-        let
-          lang_server = {
-            javascript.ts_ls.config = { };
-            bash.bashls = { };
-            nix.nixd.config = {
-              # https://github.com/nix-community/nixd/blob/main/nixd/docs/configuration.md#where-to-place-the-configuration
-              settings.nixd.formatting.command = [ "nixfmt" ];
-            };
-            rust.rust_analyzer = { };
-            yaml.yamlls = { };
-            lua.lua_ls.config.settings.Lua = ./sumneko_lua.config.lua;
-            xml.lemminx.config.filetypes = [ "xslt" ];
-            python.pyright = { };
-            json.jsonls = { };
-            html.html = { };
-            css.cssls = { };
-            cpp.clangd = { };
-            beancount.beancount.config.init_options = {
-              root_file = "Root.beancount";
-            };
-            go.gopls = { };
-            typst.tinymist = { };
-            nickel.nickel_ls = { };
-          };
-        in
-        builtins.foldl' (old: lang: old // lang_server.${lang}) { } (
-          builtins.filter hasLang (builtins.attrNames lang_server)
-        );
+      servers = lib.concatMapAttrs (_: lang: lang.servers) enabledLangs;
 
       capabilities = luaExpr "require'blink.cmp'.get_lsp_capabilities(vim.lsp.protocol.make_client_capabilities())";
 
@@ -67,62 +147,6 @@ in
       on_attach = ./lspconfig-on_attach.lua;
     };
 
-    env.PATH.values =
-      let
-        mapLangToPkgs = with pkgs; {
-          javascript = [
-            typescript-language-server
-            nodejs
-            bun
-            tsx
-          ];
-          rust = [
-            rust-analyzer
-            # default config of rust-analyzer expects cargo:
-            # https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/configs/rust_analyzer.lua#L58
-            cargo
-            # since cargo depends on rustc, lets make it available:
-            cargo.rustc
-          ];
-          nix = [
-            nixd
-            nixfmt
-          ];
-          # https://github.com/bash-lsp/bash-language-server?tab=readme-ov-file#dependencies
-          bash = [
-            bash-language-server
-            shellcheck
-            shfmt
-          ];
-          yaml = [ yaml-language-server ];
-          lua = [ lua-language-server ] ++ lib.optional (config ? wrapper.pkg.lua) config.wrapper.pkg.lua;
-          xml = [ lemminx ];
-          python = [
-            pyright
-            python3
-          ];
-          json = [ vscode-langservers-extracted ];
-          css = [ vscode-langservers-extracted ];
-          html = [ vscode-langservers-extracted ];
-          cpp = [ clang-tools ];
-          beancount = [ beancount-language-server ];
-          go = [
-            gopls
-            go
-          ];
-          typst = [
-            tinymist
-            typstyle
-            typst
-          ];
-          nickel = [
-            nls
-            nickel
-          ];
-        };
-      in
-      lib.flatten (
-        map (lang: mapLangToPkgs.${lang}) (builtins.filter hasLang (builtins.attrNames mapLangToPkgs))
-      );
+    env.PATH.values = lib.concatMap (lang: lang.packages) (lib.attrValues enabledLangs);
   };
 }
